38f383eecb47a55736a18d5ba8eb8c730a494dea
Alejandro Glez <alejandrogbarrera99@gmail.com>
2026-02-22 21:34:24 -0500

    Configure Astro deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
+  site: "https://alecog.github.io",
+  base: process.env.GITHUB_ACTIONS === "true" ? "/kairos-lab" : "/",
 });
